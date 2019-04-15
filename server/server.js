3a23e780b5ebd49028491a33acf73d35ac430333
jheltonap <j.heltonap@gmail.com>
2019-04-14 22:14:09 -0300

    adding GET\todos script

--- server/server.js
+++ server/server.js
@@ -22,5 +22,8 @@ app.post('/users', (req,res) => {
     });
     newUser.save().then((doc)=>res.send(doc), (e)=>res.status(400).send(e));
 });
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>res.send(todos), (e) => res.status(400).send(e));
+})
 
 app.listen(3000,()=>console.log('Started on port 3000'))
